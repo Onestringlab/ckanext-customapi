e6ff860a65d3fcf1bbd3eef4410d90d72f3e503c
yulrio <33221012@std.stei.itb.ac.id>
2024-11-21 15:50:16 +0700

    Update hello api

--- ckanext/customapi/plugin.py
+++ ckanext/customapi/plugin.py
@@ -1,14 +1,29 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+from ckan.model import Package
+from ckan.model.meta import Session
+from flask import Blueprint, jsonify
 
 
 class CustomapiPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IActions)
+    # plugins.implements(plugins.IBlueprint)
 
     # IConfigurer
-
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic',
-            'customapi')
+        toolkit.add_resource('fanstatic', 'customapi')
+    
+    # IActions
+    def get_actions(self):
+        return {
+            'hello_api': hello_api_action
+        }
+
+def hello_api_action(context, data_dict):
+    """
+    Endpoint sederhana
+    """
+    return {'message': 'Hello, API!', 'success': True}
