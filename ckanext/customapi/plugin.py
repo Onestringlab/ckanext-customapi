b4913d43a43c0cca0a8185c53d08061d460055a2
yulrio <33221012@std.stei.itb.ac.id>
2024-11-26 10:14:28 +0700

    Initial Commit

--- ckanext/customapi/plugin.py
+++ ckanext/customapi/plugin.py
@@ -38,7 +38,6 @@ class CustomapiPlugin(plugins.SingletonPlugin):
                 "message": "Welcome to API!",
                 "success": True
             })
-
         """
         http://localhost:5000/query-solr?q=climate&rows=5&start=10&sort=title asc
         http://localhost:5000/api/1/custom/query-solr?q=(title:Pendidikan%20AND%20notes:Pendidikan)&facet.field=[%22organization%22,%22kategori%22,%22prioritas_tahun%22,%22tags%22,%22res_format%22]&facet.limit=500&start=0&rows=20&sort=prioritas_tahun%20desc&include_private=true
@@ -76,7 +75,7 @@ class CustomapiPlugin(plugins.SingletonPlugin):
                     'sort': sort,
                     'facet': 'true',
                     'facet.limit': facet_limit,
-                    'fq': 'private:true' if include_private else '-private:true',
+                    'fq': 'private:true' if include_private else '-private:true'
                 }
 
                 # Tambahkan setiap facet.field secara terpisah
