da0af097c6d1c614063ba1d44579390d79a1e023
yulrio <33221012@std.stei.itb.ac.id>
2024-11-26 11:27:05 +0700

    Initial Commit

--- ckanext/customapi/plugin.py
+++ ckanext/customapi/plugin.py
@@ -65,7 +65,7 @@ class CustomapiPlugin(plugins.SingletonPlugin):
                     'facet': 'true',
                     'facet.field': ['organization','kategori'],  # Field untuk faceting
                     'facet.limit': facet_limit,
-                    'include_private':true
+                    # 'include_private':true
                 }
 
                 # Kirim query ke Solr
