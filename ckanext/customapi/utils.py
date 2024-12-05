dda54b1b6f42e0ce0ca1c1c3660dc75ed330fd3f
yulrio <33221012@std.stei.itb.ac.id>
2024-12-06 05:49:38 +0700

    Initial Commit

--- ckanext/customapi/utils.py
+++ ckanext/customapi/utils.py
@@ -5,6 +5,6 @@ def query_database(query, params=None):
     Helper function untuk menjalankan query ke database CKAN.
     """
     session = meta.Session
-    result = session.execute(query, params or {})
+    # result = session.execute(query, params or {})
     # return result.fetchall()
     return query
