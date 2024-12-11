5e227ebd4aca69d083e214ed19240efbfbb64c74
yulrio <33221012@std.stei.itb.ac.id>
2024-12-11 13:10:41 +0700

    Initial Commit

--- ckanext/customapi/plugin.py
+++ ckanext/customapi/plugin.py
@@ -177,7 +177,7 @@ class CustomapiPlugin(plugins.SingletonPlugin):
                     'facet': 'true',
                     'facet.field': ['organization', 'kategori', 'prioritas_tahun', 'tags', 'res_format'],
                     'facet.limit': facet_limit,
-                    'include_private': False 
+                    'include_private': True 
                 }
 
                 context = {'ignore_auth': False}
