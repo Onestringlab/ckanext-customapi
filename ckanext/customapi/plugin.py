a35cd5aab1628233d50387a3e00478b00c530524
yulrio <33221012@std.stei.itb.ac.id>
2024-12-20 21:39:07 +0700

    Initial Commit

--- ckanext/customapi/plugin.py
+++ ckanext/customapi/plugin.py
@@ -48,7 +48,7 @@ class CustomapiPlugin(plugins.SingletonPlugin):
             Route untuk /welcome_api
             """
             return jsonify({
-                "message": "Welcome to the Virtual World 5!!",
+                "message": "Welcome to the Virtual World 6!!",
                 "success": True
             })
 
