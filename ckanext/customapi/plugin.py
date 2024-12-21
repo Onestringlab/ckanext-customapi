ad857cb5c239559a5a0fa17af929c4dc4c0a13b7
yulrio <33221012@std.stei.itb.ac.id>
2024-12-21 23:27:55 +0700

    Initial Commit

--- ckanext/customapi/plugin.py
+++ ckanext/customapi/plugin.py
@@ -1,5 +1,5 @@
-import requests
 import jwt
+import requests
 from datetime import datetime
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
@@ -235,8 +235,8 @@ class CustomapiPlugin(plugins.SingletonPlugin):
         @blueprint_customapi.route('/get-data-organizations', methods=['POST'])
         def get_data_organizations():
             try:
-                result = list_organizations()
-                return jsonify({"success": True, "data": result})
+                data_kl = list_organizations()
+                return jsonify({"success": True, "total_kl": len(data_kl), "data_kl": data_kl})
             except Exception as e:
                 return jsonify({"error": f"{str(e)}"}), 400
 
