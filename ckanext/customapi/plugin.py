699214a705a9ee1128e16c8ba1737032cadfc119
yulrio <33221012@std.stei.itb.ac.id>
2025-01-05 14:00:58 +0700

    Initial Commit

--- ckanext/customapi/plugin.py
+++ ckanext/customapi/plugin.py
@@ -200,7 +200,7 @@ class CustomapiPlugin(plugins.SingletonPlugin):
                 if request_name:
                     dataset_id = request_name
 
-                params = {'id': dataset_id}
+                params = {'id': dataset_id, 'include_dataset': True}
 
                 has_access = has_package_access(username, dataset_id)
 
