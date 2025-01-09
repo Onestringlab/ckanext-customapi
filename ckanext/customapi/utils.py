ba0a606a73486e5edbdc196abcaa9c0e88472047
yulrio <33221012@std.stei.itb.ac.id>
2025-01-09 22:51:49 +0700

    Initial Commit

--- ckanext/customapi/utils.py
+++ ckanext/customapi/utils.py
@@ -306,7 +306,7 @@ def get_organizations_query(q, sort,limit=10, offset=0):
                 AND g.approval_status = 'approved'
                 AND g.title LIKE :q
                 AND p.state = 'active'
-                AND (p.type ='dataset' OR p.type ='kurasi_stages')
+                AND p.type ='dataset'
                 GROUP BY g.id, g.name, g.title, g.image_url
                 ORDER BY g.title {sort}
                 LIMIT :limit
