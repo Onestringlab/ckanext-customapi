6a69b4b62aa28932ebed35eefca964646732e545
yulrio <33221012@std.stei.itb.ac.id>
2025-01-14 16:41:46 +0700

    Initial Commit

--- ckanext/customapi/plugin.py
+++ ckanext/customapi/plugin.py
@@ -300,7 +300,7 @@ class CustomapiPlugin(plugins.SingletonPlugin):
                 for activity in response:
                     user_id = activity.get("user_id")
                     if user_id:
-                        user_profile = get_action('user_show')(context,{"id":user_id})
+                        user_profile = get_profile_by_id(user_id)
                         activity["user_profile"] = user_profile
 
                 return jsonify({"success": True, "email": email, "data": response})
