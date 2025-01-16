7c9163ee8cfbb8c17da9a15dc4710227d04929c2
yulrio <33221012@std.stei.itb.ac.id>
2025-01-16 09:53:44 +0700

    Initial Commit

--- ckanext/customapi/utils.py
+++ ckanext/customapi/utils.py
@@ -96,8 +96,10 @@ def get_profile_by_id(user_id):
                     apikey, created, fullname, email, sysadmin, state, image_url 
                 FROM public.user 
                 WHERE id = :user_id
+                LIMIT 1
             '''
     result = query_custom(query, {'user_id': user_id})
+    row = result[0]
 
     # Konversi hasil query menjadi daftar dictionary
     data = [
