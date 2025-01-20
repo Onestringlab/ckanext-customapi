1265ba1daf9b87ee29a6cf225d037345bf5e881f
yulrio <33221012@std.stei.itb.ac.id>
2025-01-20 15:25:25 +0700

    Initial Commit

--- ckanext/customapi/plugin.py
+++ ckanext/customapi/plugin.py
@@ -446,7 +446,7 @@ class CustomapiPlugin(plugins.SingletonPlugin):
                 #     return data["response"]["docs"]
                 # else:
                 #     return []  # Jika tidak ada hasil ditemukan
-                return "data"
+                return data
 
             except requests.RequestException as e:
                 print(f"Error fetching similar datasets: {e}")
