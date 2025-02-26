9811498dc005b2ac9dfd73503ff67cf9325f3423
Yulrio Brianorman <rionorman@gmail.com>
2025-02-26 13:31:54 +0700

    Initial Commit

--- ckanext/customapi/plugin.py
+++ ckanext/customapi/plugin.py
@@ -50,7 +50,7 @@ class CustomapiPlugin(plugins.SingletonPlugin):
             """
             Route untuk /welcome_api
             """
-            message = "Welcome to the Virtual World 26.02!"
+            message = "Welcome to the Virtual World 26.2!"
             log.info(f'message:{message}')
 
             # Buat respons JSON
