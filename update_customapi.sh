3849a9feca548ec53e8e24372f175289ba50b887
Yulrio Brianorman <rionorman@gmail.com>
2025-02-26 11:45:45 +0700

    Initial Commit

--- update_customapi.sh
+++ update_customapi.sh
@@ -4,10 +4,12 @@
 COMMIT_MESSAGE="Initial Commit"
 
 # Perintah git di host
-cd "$HOST_EXT_PATH" || exit
 git add .
 git commit -m "$COMMIT_MESSAGE"
 git push -u origin main
+
+# URL repository
+REPO_URL="https://repo.tech-dev.id/sdi/ckan/ckanext-customapi.git"
 
 # Nama container Docker
 CONTAINER_NAME="ckan"
@@ -17,9 +19,8 @@ EXT_PATH="/srv/app/ext_2024/ckanext-customapi"
 
 # Perintah untuk menjalankan pembaruan
 docker exec "$CONTAINER_NAME" bash -c "
-    cd $EXT_PATH &&
-    git pull &&
-    pip install -e .
+    [ ! -d $EXT_PATH ] && git clone $REPO_URL $EXT_PATH
+    cd $EXT_PATH && git pull && pip install -e .
 "
 
 # Restart container (opsional, hapus jika tidak diperlukan) #
